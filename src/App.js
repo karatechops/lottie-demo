bd106cab3712142f0df1d90ace1f811784d6f0d6
karatehcops <contact@lazers.tv>
2019-09-06 12:52:43 -0400

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import lottie from "lottie-web";
+import "./App.css";
 
 function App() {
+  const animContainer = React.createRef();
+
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: animContainer.current, // the dom element that will contain the animation
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      path: "/name-anim.json" // the path to the animation json
+    });
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div
+          id="anim"
+          ref={animContainer}
+          style={{ height: "300px", width: "100%" }}
+        />
+        <p>Lottie + After Effects Demo</p>
       </header>
     </div>
   );
